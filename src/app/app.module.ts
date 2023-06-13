7660d54e0a2111ad01246b48144adfd632fd5ce9
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-13 22:22:30 +0100

    Step 10 Add routes to the application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { provideRouter } from '@angular/router';
+import routeConfig from './routes';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { HousingLocationComponent } from './housing-location/housing-location.component';
+import { DetailsComponent } from './details/details.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    HousingLocationComponent
+    HousingLocationComponent,
+    DetailsComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [ provideRouter(routeConfig)],
   bootstrap: [AppComponent,
     HomeComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  
+}
