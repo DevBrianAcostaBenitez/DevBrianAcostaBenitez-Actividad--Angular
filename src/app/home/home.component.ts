01ecbc237dca744976e9641e314ed531bd30b66c
DevBrianAcostaBenitez <brianacostabenitez@gmail.com>
2023-06-13 14:57:14 +0100

    Step 4 Creating an interface

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
-
+import { HousingLocation } from '../housinglocation';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: 'Test Home',
+    city: 'Test city',
+    state: 'ST',
+    photo: 'assets/example-house.jpg',
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
 }
